009696e4aaaca4897941bcd389ea577b2575b9d1
shivramwarchetan <shivramwarchetan@gmail.com>
2024-09-18 15:30:01 +0530

    first

--- app/page.js
+++ app/page.js
@@ -1,101 +1,86 @@
 import Image from "next/image";
+import ArrowLeftIcon from "../public/icons/leftIcon";
+
+const services = [
+  {
+    label: "Industry Experts",
+    bgColor: "bg-[#B22222]",
+    textColor: "text-white",
+  },
+  { label: "Dedicated Team", bgColor: "bg-slate-300", textColor: "text-black" },
+  {
+    label: "Outcome focused",
+    bgColor: "bg-slate-300",
+    textColor: "text-black",
+  },
+  {
+    label: "High Quality Service",
+    bgColor: "bg-slate-300",
+    textColor: "text-black",
+  },
+  {
+    label: "Cyber Security Expert",
+    bgColor: "bg-slate-300",
+    textColor: "text-black",
+  },
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="bg-white text-black justify-center items-center text-center p-6 min-h-screen">
+      <div className="text-gray-600 text-lg p-2">WHY CHOOSE US</div>
+      <div className="text-black text-4xl font-bold p-2">
+        We Are Different From Others
+      </div>
+      <div className="text-gray-600 p-2 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa cupiditate
+        accusantium recusandae soluta explicabo hic!
+      </div>
+      <div className="flex flex-col lg:flex-row justify-between pl-6 pr-6 p-2">
+        <div className="relative flex flex-col lg:flex-row items-center lg:items-start">
+          <div className="bg-[#CD5C5C] rounded-full text-white h-96 w-96 flex flex-col justify-center items-center text-center space-y-4 z-20 opacity-80">
+            <div className="text-xl font-bold">XYZ Industry Experts</div>
+            <div className="text-lg text-gray-100 px-6">
+              Made by Sushil Gupta. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Ipsa cupiditate accusantium recusandae soluta
+              explicabo hic!
+            </div>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <div className="rounded-full text-white h-96 w-96 absolute top-0 lg:left-[16rem]">
             <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className="rounded-full"
+              src="/images/Laptop.png"
+              layout="fill"
+              objectFit="cover"
+              priority
+              alt="Picture of the author"
+              sizes="(max-width: 768px) 100vw, (min-width: 769px) 50vw"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="flex justify-between lg:pr-16 p-2 pt-12 lg:pt-24">
+          <ul>
+            {services.map((service, index) => (
+              <li
+                key={index}
+                className={`text-lg font-bold ${service.bgColor} ${service.textColor} w-full lg:w-96 rounded-l-full m-2`}
+              >
+                <button
+                  type="button"
+                  className="flex justify-between p-2 w-full"
+                >
+                  <ArrowLeftIcon
+                    className={`fill-current ${service.textColor}`}
+                  />
+                  {service.label}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ public/icons/leftIcon.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+const ArrowLeftIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    width={20}
+    height={25}
+    viewBox="0 0 330.002 330.002"
+    {...props}
+  >
+    <path d="M229.966.847a15 15 0 0 0-16.678 4.784l-120 150.004a15 15 0 0 0 .001 18.741l120 149.996a15 15 0 0 0 26.713-9.371v-300A15 15 0 0 0 229.966.847z" />
+  </svg>
+);
+export default ArrowLeftIcon;
